0e68b396e1ccd53077fd8d55d1ab535903235b1c
Robert Tarnas <robert.tarnas@protonmail.com>
2021-03-04 00:35:01 +0100

    fix

--- Greande Haskell /cbits/gradient_descent.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-void descend_cpu(int len, double rate, double momentum, double regulariser,
-    const double* weights,
-    const double* gradient,
-    const double* last,
-    double* outputWeights, double* outputMomentum);
-
--- Greande Haskell /src/Grenade/Recurrent/Core.hs
+++
@@ -1,9 +0,0 @@
-module Grenade.Recurrent.Core (
-    module Grenade.Recurrent.Core.Layer
-  , module Grenade.Recurrent.Core.Network
-  , module Grenade.Recurrent.Core.Runner
-  ) where
-
-import           Grenade.Recurrent.Core.Layer
-import           Grenade.Recurrent.Core.Network
-import           Grenade.Recurrent.Core.Runner
