b8bdfcb012a909fb27569980939c9ae93e153271
Robert Tarnas <robert.tarnas@protonmail.com>
2021-04-13 15:42:19 +0200

    update

--- klasyfikator.hs
+++ klasyfikator.hs
@@ -8,6 +8,15 @@ takt4 = line [e 4 sn, e 4 sn, e 4 sn, e 4 sn,  d 4 sn, c 4 sn]
 takt5 = line [b 4 sn,  d 4 sn,  d 4 sn, d 4 sn, d 4 sn, c 4 sn]
 takt6 = line [a 4 sn, b 4 sn, c 4 qn, rest qn, c 4 qn, rest qn, a 4 hn]
 
-test = takt1 :+: takt2 :+: takt3 :+: takt4 :+: takt5 :+: takt6
+--test = takt1 :+: takt2 :+: takt3 :+: takt4 :+: takt5 :+: takt6
+
+-- konwersja na AbsolutePitch:
+
+linia1 = [69,69,69,68,64]
+lina2 = [69,69,69,71,68]
+linia3 = [69, 69, 60,62, 64]
+linia4 = [64, 64,64,64,62,60]
+linia5 = [71,62,62,62,62,60]
+linia6 = [69,71,60,60,69]
 
 
