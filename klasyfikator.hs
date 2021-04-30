f5bd4683979138dc7d1ad746a52a9f2b4a84507e
Robert Tarnas <robert.tarnas@protonmail.com>
2021-04-30 19:43:03 +0200

    Finished classifier

--- klasyfikator.hs
+++ klasyfikator.hs
@@ -1,4 +1,7 @@
--- http://oskarkolberg.pl/en-US;q=0.7/MusicDb/Details/cafbb535-ed69-4b99-9002-f65fe40ed8fe
+import System.Random
+import Test.QuickCheck (generate, elements)
+
+-- 1 utwor: http://oskarkolberg.pl/en-US;q=0.7/MusicDb/Details/cafbb535-ed69-4b99-9002-f65fe40ed8fe
 -- "Od Pilicy i Wolbroma"
 import Euterpea
 takt1 = line [a 4 qn, rest qn, a 4 sn, a 4 sn, gs 4 sn,  e 4 sn]
@@ -8,21 +11,55 @@ takt4 = line [e 4 sn, e 4 sn, e 4 sn, e 4 sn,  d 4 sn, c 4 sn]
 takt5 = line [b 4 sn,  d 4 sn,  d 4 sn, d 4 sn, d 4 sn, c 4 sn]
 takt6 = line [a 4 sn, b 4 sn, c 4 qn, rest qn, c 4 qn, rest qn, a 4 hn]
 
-test = takt1 :+: takt2 :+: takt3 :+: takt4 :+: takt5 :+: takt6
+utwor1 = takt1 :+: takt2 :+: takt3 :+: takt4 :+: takt5 :+: takt6
+
+jazz = line [a 5 en, f 5 en, g 5 en, f 5 en, a 5 qn, f 5 qn, a 5 qn, f 5 qn, g 5 qn, a 5 qn, f 5 qn, g 5 qn, f 5 qn]
+
 
 -- konwersja na AbsolutePitch:
 
 linia1 = [69,69,69,68,64]
-lina2 = [69,69,69,71,68]
+linia2 = [69,69,69,71,68]
 linia3 = [69, 69, 60,62, 64]
 linia4 = [64, 64,64,64,62,60]
 linia5 = [71,62,62,62,62,60]
 linia6 = [69,71,60,60,69]
 
+v = [81, 77, 79, 77, 81, 77, 81, 77, 79, 81, 77, 79, 77] 
+u = [69,69,69,68,64,69,69,69,71,68, 69, 69, 60] 
 
---klasyfikator:
 
--- definicje funkcji klasyfikujacych:
-g1 :: [Int] -> [Int] -> [Int]
-g1 xs xs =
-    return  xs[0] + xs[1]
+x = [66,68..138] 
+
+
+w_0 :: Int
+w_0 = (dot v v   - dot u v )  `div` 2
+
+y = odejmij v u
+
+y_x = dot y x
+
+
+
+
+
+-- Funkcje
+
+
+-- odejmowanie dwoch wektorow
+odejmij :: [Int] -> [Int] -> [Int]
+odejmij [] _ = []
+odejmij _ [] = []
+odejmij (x:xs) (y:ys) = x - y : (odejmij xs ys)
+
+
+-- Iloczyn skalarny
+dot :: [Int] -> [Int] -> Int
+dot x y = sum $ zipWith (*) x y
+
+klasyfikacja:: Int -> Int 
+klasyfikacja y_x
+    | y_x > w_0 = 1 -- klasa C1
+    | y_x < w_0 = -1 --klasa C2
+
+
