39acb015e5d4c572def75def6860dd833a3c91df
Robert Tarnas <robert.tarnas@protonmail.com>
2021-05-05 09:00:39 +0200

    Fixes

--- Klasyfikator/klasyfikator.hs
+++ Klasyfikator/klasyfikator.hs
@@ -1,9 +1,10 @@
 import System.Random
 import Test.QuickCheck (generate, elements)
+import Euterpea
 
 -- 1 utwor: http://oskarkolberg.pl/en-US;q=0.7/MusicDb/Details/cafbb535-ed69-4b99-9002-f65fe40ed8fe
 -- "Od Pilicy i Wolbroma"
-import Euterpea
+
 takt1 = line [a 4 qn, rest qn, a 4 sn, a 4 sn, gs 4 sn,  e 4 sn]
 takt2 = line [a 4 qn, rest qn, a 4 sn, a 4 sn, b 4 sn, gs 4 sn]
 takt3 = line [a 4 sn, a 4 sn, c 4 qn, d 4 qn, e 4 hn]
